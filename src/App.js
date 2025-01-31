6e03a527abde80db7b58a80857e7ff08726c1d59
Suppyour <mporsev@mail.ru>
2025-01-31 17:37:09 +0400

    Инициализировался

---
+++ src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <img src="/logo192.png" alt="Logo" className="logo"/>      
+      <header>
+        <div className="menu">
+          <a>Главная</a>
+          <a>Услуги</a>
+          <a>Портфолио</a>
+          <a>Нормативные документы</a>
+          <a>Контакты</a>
+          <a>Оборудование</a>
+          <a>Вакансии</a>
+        </div>
+
+        <button className="btn">Заказать звонок</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;

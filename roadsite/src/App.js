dab98de71fac49b042cfb6cf8a6bf4329d54bb47
Suppyour <mporsev@mail.ru>
2025-02-03 21:00:56 +0400

    Header белый + фоновое видево (#2)

--- roadsite/src/App.js
+++ roadsite/src/App.js
@@ -1,21 +1,47 @@
-function App() {
-  return (
-    <div>
-      <img src="/logo192.png" alt="Logo" className="logo"/>      
-      <header>
-        <div className="menu">
-          <a>Главная</a>
-          <a>Услуги</a>
-          <a>Портфолио</a>
-          <a>Нормативные документы</a>
-          <a>Контакты</a>
-          <a>Оборудование</a>
-          <a>Вакансии</a>
-        </div>
+import { useRef, useEffect } from 'react';
 
-        <button className="btn">Заказать звонок</button>
-      </header>
-    </div>
+function App() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      videoElement.muted = true;
+      const playPromise = videoElement.play();
+
+      if (playPromise !== undefined) {
+        playPromise.catch(error => {
+          console.error('Autoplay prevented:', error);
+        });
+      }
+    }
+  }, []);
+
+  return (
+      <div>
+        <img src="/logo192.png" alt="Logo" className="logo" />
+        <header>
+          <div className="menu">
+            <a>Главная</a>
+            <a>Услуги</a>
+            <a>Портфолио</a>
+            <a>Нормативные документы</a>
+            <a>Контакты</a>
+            <a>Оборудование</a>
+            <a>Вакансии</a>
+          </div>
+          <button className="btn">Заказать звонок</button>
+        </header>
+        <div className="video">
+            <video autoPlay muted loop>
+                <source src="/media1.mp4" type="video/mp4"/>
+                Ваш браузер не поддерживает тег video.
+            </video>
+        </div>
+          <div>
+
+          </div>
+      </div>
   );
 }
 

3788b04e88cd88cf2120cc6545e2aadf31cd3eb0
Suppyour <mporsev@mail.ru>
2025-03-21 00:26:42 +0500

    Играл с полями в Modal (#23)

--- roadsite/src/Common/Modal/Modal.jsx
+++ roadsite/src/Common/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Modal.css"
+import { Drawer } from "@chakra-ui/react"
 
 const Modal = ({active, setActive, children}) => {
     console.log(active); // Отладка: значение active
@@ -7,9 +8,21 @@ const Modal = ({active, setActive, children}) => {
     return (
         <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
             <div className="modal__content" onClick={e => e.stopPropagation()}>
-                {children}
+                <div className="joepeech">
+                    <a>Как вас зовут</a>
+                    <input type="text" placeholder="Как вас зовут))" />
+<a>Че хотите по услугам</a>
+                    <input type="text"></input>
+                    <a>Номерок запиши сюда</a>
+
+                    <input type="text"></input>
+                    <input type="text"></input>
+                    <input type="text"></input>
+                    <input type="radio"></input>
+                </div>
             </div>
         </div>
+
     );
 }
 

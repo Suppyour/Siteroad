81d350f330411a3f56acbcc9c1b8038c19a9ac05
Suppyour <mporsev@mail.ru>
2025-03-22 19:37:36 +0500

    Для ильи ()

--- roadsite/src/Pages/Contacts/Contacts.js
+++ roadsite/src/Pages/Contacts/Contacts.js
@@ -1,6 +1,6 @@
     import React from 'react';
     import './Contacts.css';
-
+    
     function Contacts() {
         return (
             <section className="contacts">
@@ -19,7 +19,7 @@
                 <ul className= "list">
                 <li>Адрес : 614109, Пермский край , г. Пермь</li>
                 <li>
-                    <a href="https://yandex.ru/maps/-/CHBZjCYI" target="_blank" style={{color: "grey"}} >Кировоградская д.180 кв 17</a>
+                    <a href="https://yandex.ru/maps/50/perm/house/kirovogradskaya_ulitsa_180/YU8YdgdkSUcOQFtifXxweX5qZw==/panorama/?l=stv%2Csta&ll=56.004421%2C58.017838&panorama%5Bdirection%5D=116.569737%2C5.407209&panorama%5Bfull%5D=true&panorama%5Bpoint%5D=56.003678%2C58.018482&panorama%5Bspan%5D=98.926618%2C60.000000&tab=panorama&utm_source=share&z=17" target="_blank" style={{color: "grey"}} >Кировоградская д.180 кв 17</a>
                 </li>
                 </ul>
             </section>

2c648dfd858be609a65c6debb4c33ad2c2d8a6c8
Suppyour <mporsev@mail.ru>
2025-03-30 18:37:13 +0500

    Адаптивность, логотип, исправления (#28)

--- roadsite/src/App.js
+++ roadsite/src/App.js
@@ -1,6 +1,6 @@
 import './Common/video.css';
 import './Common/footer.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './Common/Footer';
 import Home from './Pages/General/Home';
 import Services from './Pages/Service/Services';
@@ -17,28 +17,34 @@ import Ksodd from "./Pages/Service/Support/ksodd";
 import Navbar from "./Common/Navbar";
 import GeographyWork from "./Pages/GeoraphyWork/GeographyWork";
 import React from "react";
-
+import './App.css';
 
 function App() {
-    return (<Router>
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/services" element={<Services/>}/>
-            <Route path="/portfolio" element={<Portfolio/>}/>
-            <Route path="/documents" element={<Documents/>}/>
-            <Route path="/contacts" element={<Contacts/>}/>
-            <Route path="/vacancies" element={<Vacancies/>}/>
-            <Route path="/diagnostic" element={<Diagnostic/>}/>
-            <Route path="/passport" element={<Passport/>}/>
-            <Route path="/georadar" element={<Georadar/>}/>
-            <Route path="/monitoring" element={<Monitoring/>}/>
-            <Route path="/axial_loads" element={<Axial/>}/>
-            <Route path="/ksodd" element={<Ksodd/>}/>
-            <Route path="/regions" element={<GeographyWork/>}/>
-        </Routes>
-        <Footer/>
-    </Router>);
+    return (
+        <Router>
+            <div className="page-container">
+                <main className="content">
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/services" element={<Services />} />
+                        <Route path="/portfolio" element={<Portfolio />} />
+                        <Route path="/documents" element={<Documents />} />
+                        <Route path="/contacts" element={<Contacts />} />
+                        <Route path="/vacancies" element={<Vacancies />} />
+                        <Route path="/diagnostic" element={<Diagnostic />} />
+                        <Route path="/passport" element={<Passport />} />
+                        <Route path="/georadar" element={<Georadar />} />
+                        <Route path="/monitoring" element={<Monitoring />} />
+                        <Route path="/axial_loads" element={<Axial />} />
+                        <Route path="/ksodd" element={<Ksodd />} />
+                        <Route path="/regions" element={<GeographyWork />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- roadsite/src/Common/Navbar.js
+++ roadsite/src/Common/Navbar.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './Navbar.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <header className="navbar">
             <nav className="navbar-nav">
                 <ul className="navbar-list">
-                    <Link to="/" className= "navbar-button">Главная</Link>
-                    <Link to="/services" className="navbar-button">Услуги</Link>
-                    <Link to="/documents" className="navbar-button">Документы</Link>
-                    <Link to="/regions" className="navbar-button"
-                          style={{ paddingTop: "0.2vh" }}>
-                        География работ
-                    </Link>
-                    <Link to="/contacts" className="navbar-button">Контакты</Link>
+                    <li>
+                        <Link to="/" className="navbar-button">Главная</Link>
+                    </li>
+                    <li>
+                        <Link to="/services" className="navbar-button">Услуги</Link>
+                    </li>
+                    <li>
+                        <Link to="/documents" className="navbar-button">Документы</Link>
+                    </li>
+                    <li>
+                        <Link to="/regions" className="navbar-button" style={{ paddingTop: "0.3vh" }}>География работ</Link>
+                    </li>
+                    <li>
+                        <Link to="/contacts" className="navbar-button">Контакты</Link>
+                    </li>
                 </ul>
                 <div className="navbar-logo">
-                    <img src="/logo/logo192_negate.png" alt="WhatsApp" />
+                    <img src="/logo/logo192_negate.png" alt="Logo" />
                 </div>
             </nav>
         </header>
-
     );
 };
 
